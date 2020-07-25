1517edd187c77a62ac7d4e2c779230b6c0f3fdbc
Keadex <12099977+keadex@users.noreply.github.com>
2020-07-26 00:23:29 +0200

    updated infrastructure architecture component with new structure

--- src/components/architecture/InfrastructureArchitecture.tsx
+++ src/components/architecture/InfrastructureArchitecture.tsx
@@ -1,53 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import InfrArchSvg from '../../assets/img/infrastructure-architecture.svg'; 
-import { ReactSVG } from 'react-svg'
+import React from 'react';
 import "./Architecture.scss";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { MDBIcon } from 'mdbreact';
-import { MDBBtn } from 'mdbreact';
-import { MDBBtnGroup } from 'mdbreact';
-import { FormattedMessage } from 'react-intl';
-import { ArchitectureChalkboardToolbar } from './ArchitectureChalkboardToolbar';
+import { Architecture, ArchitectureModule, ArchitectureImplProps } from './Architecture';
 import { ArchitectureType } from '../../pages/kea-lab/KeaLab';
-// import { ArchitectureType } from './Architecture';
+import { useIntl } from 'react-intl';
+import InfrArchSvg from '../../assets/img/infrastructure-architecture.svg'; 
 
 
 //------------------ TYPES
-interface InfrastructureArchitectureProps{
-  architectureType: ArchitectureType;
-  onArchitectureTypeSelection: (architectureType: ArchitectureType)=>void;
-}
+
 
 //------------------ COMPONENT
-export const InfrastructureArchitecture : React.FunctionComponent<InfrastructureArchitectureProps> = (props:InfrastructureArchitectureProps) => {
-
-  //----- afterInjection
-  function afterInjection(error:Error|null, svg:SVGElement|undefined):void{
-    if (svg){
-    // props.svgPanProps.resetTransform();
-    // props.svgPanProps.resetTransform();
-      // let battistiModule = svg.querySelector("#battisti-module");
-      // battistiModule!.classList.add("architecture__module");
-      // battistiModule!.addEventListener("click", ()=>{console.log("ciao")});
-    }
-  }
+export const InfrastructureArchitecture : React.FunctionComponent<ArchitectureImplProps> = (props:ArchitectureImplProps) => {
+  
+  const intl = useIntl();
+  
+  let modules : ArchitectureModule[] = []
 
 
   //----- render
   return (
     <React.Fragment>
-      <div className="architecture__chalkboard-container">
-        <TransformWrapper defaultScale={1}>
-          {(zoomPanPinchProps:any) => (
-            <React.Fragment>
-              {/* <ArchitectureChalkboardToolbar modules={[]} onArchitectureTypeSelection={props.onArchitectureTypeSelection} moduleDetailsRef={React.createRef()} architectureType={props.architectureType} zoomPanPinchProps={zoomPanPinchProps} /> */}
-              <TransformComponent>
-                <ReactSVG src={InfrArchSvg} renumerateIRIElements={false} afterInjection={afterInjection} className={"architecture__chalkboard"}/>
-              </TransformComponent>
-            </React.Fragment>
-          )}
-        </TransformWrapper>
-      </div>
+      <Architecture 
+        architectureType={ArchitectureType.infrastructure} 
+        onArchitectureTypeSelection={props.onArchitectureTypeSelection}
+        modules={modules}
+        chalkboard={InfrArchSvg} />
     </React.Fragment>
   );
 }
