1ec7e71bd0fcbca1046db5e31ff503aeb36ae97f
Keadex <12099977+keadex@users.noreply.github.com>
2020-07-26 02:24:45 +0200

    replaced loader

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -28,36 +28,14 @@ class Spinner extends React.Component<ISpinnerProps> {
   //------------ render
   public render() {
     return (
-        <div className={this.isSpinnerVisible()?"spinner__container spinner--visible":"spinner__container spinner--hidden"} >
-          {/* <div className="spinner-border text-danger spinner" role="status">
-            <span className="sr-only">Loading...</span>
-          </div> */}
-          <div className="cssload-loading">
-            <div className="cssload-finger cssload-finger-1">
-              <div className="cssload-finger-item">
-                <span></span><i></i>
-              </div>
-            </div>
-            <div className="cssload-finger cssload-finger-2">
-              <div className="cssload-finger-item">
-                <span></span><i></i>
-              </div>
-            </div>
-            <div className="cssload-finger cssload-finger-3">
-              <div className="cssload-finger-item">
-                <span></span><i></i>
-              </div>
-            </div>
-            <div className="cssload-finger cssload-finger-4">
-              <div className="cssload-finger-item">
-                <span></span><i></i>
-              </div>
-            </div>
-            <div className="cssload-last-finger">
-              <div className="cssload-last-finger-item"><i></i></div>
-            </div>
-          </div>
-        </div>        
+      <div className={this.isSpinnerVisible()?"spinner__container spinner--visible":"spinner__container spinner--hidden"} >
+        <div className="lds-ellipsis full-center position-absolute">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>        
     );
   }
 }
