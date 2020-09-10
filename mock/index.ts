7712e08d18d1f21c566f5a62a550504a58357598
Keadex <12099977+keadex@users.noreply.github.com>
2020-09-10 03:27:39 +0200

    added experiences GraphQL mock

--- mock/index.ts
+++ mock/index.ts
@@ -1,36 +1,55 @@
 import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer, gql, addMockFunctionsToSchema } from "apollo-server-express";
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { join } from "path";
+import faker from 'faker';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
 const PORT = 4000;
 
 const app = express();
 
-const typeDefs = gql`
-type Cat {
-  id: ID!
-  name: String!
-  age: Int!
-  nice: Boolean
-}
-type Horse {
-  id: ID!
-  name: String!
-  netWorth: Float!
-  description: String
-}
-type Query {
-  allCats: [Cat!]!
-  allHorses: [Horse!]!
-}
-`;
+const schema = loadSchemaSync(join(__dirname, 'schema.graphql'), { loaders: [new GraphQLFileLoader()] });
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!'
-  },
+// const typeDefs = gql`
+// type Cat {
+//   id: ID!
+//   name: String!
+//   age: Int!
+//   nice: Boolean
+// }
+// type Horse {
+//   id: ID!
+//   name: String!
+//   netWorth: Float!
+//   description: String
+// }
+// type Query {
+//   allCats: [Cat!]!
+//   allHorses: [Horse!]!
+// }
+// `;
+
+
+// const CatResolver = {
+//   Cat: {
+//     id: () => '123'
+//   },
+// };
+
+const mocks = {
+  // Horse: () => ({
+  //   description: () => faker.random.arrayElement(["ciao1", "coap2", "ciao3"])
+  // }),
+  Query: () => ({
+    experiences: ()=> require('./mock-responses/get-experience.json')
+  })
 };
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+const server = new ApolloServer({ schema,  mocks });
 server.applyMiddleware({ app });
 
 app.listen({ port: PORT }, () => {
