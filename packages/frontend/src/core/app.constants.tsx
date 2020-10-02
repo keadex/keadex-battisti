1d6e81ed5d139cd28dadcde408a3610d9d56cabb
Keadex <12099977+keadex@users.noreply.github.com>
2020-10-03 01:45:06 +0200

    minor improvements

--- packages/frontend/src/core/app.constants.tsx
+++ packages/frontend/src/core/app.constants.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 //GENERIC
 export const MAX_BOT_MESSAGES = 5;
 export const DEFAULT_REVALIDATE_SECONDS = 60;
+export const GITHUB_MASTER_BASEURL:string = process.env.NEXT_PUBLIC_GITHUB_MASTER_BASEURL!;
+
 
 export enum SceneId {
   Education = "EDUCATION",
