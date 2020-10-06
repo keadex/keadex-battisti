8b13dd78bb733189cea1fd08a5fc4a7442d2877d
Keadex <12099977+keadex@users.noreply.github.com>
2020-10-06 21:55:01 +0200

    added SSG

--- packages/frontend/src/pages/404.tsx
+++ packages/frontend/src/pages/404.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import PageLayout from "../components/page-layout/page-layout";
 import notFoundImage from '../../public/img/404.svg'; 
+import { GetStaticProps } from "next";
+import { wrapper } from "../core/store/store";
+
+//---------- getStaticProps
+export const getStaticProps: GetStaticProps = wrapper.getStaticProps(
+  async ({store}) => {
+    return {
+      props: {}
+    }
+  }
+);
 
 //------------------ COMPONENT
 const CookieDeclaration : React.FunctionComponent<any> = () => {
