9244d23e772eb2709d03a6970177054986609d99
Keadex <12099977+keadex@users.noreply.github.com>
2020-10-07 03:12:28 +0200

    Update server.js

--- packages/cms/config/server.js
+++ packages/cms/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "/cms",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'random-secret-token'),
