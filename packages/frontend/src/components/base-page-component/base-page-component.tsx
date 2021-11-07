e3d18d2ac3cd7e58bfebf7a55b067bbc4d1e5119
Keadex <12099977+keadex@users.noreply.github.com>
2021-11-07 20:25:48 +0100

    removed unuseful comments

--- packages/frontend/src/components/base-page-component/base-page-component.tsx
+++ packages/frontend/src/components/base-page-component/base-page-component.tsx
@@ -19,18 +19,9 @@ class BasePageComponent<K,V> extends React.Component<K,V> {
 
   //------------ scrollToHash
   scrollToHash(){
-    // console.log(StoreService.getInstance().getStore()?.getState().app.menuOpen);
     let hash = window.location.hash?window.location.hash.replace("#", ""):undefined;
-    // let page = window.location.pathname.replace("/", "");
-    // console.log(hash + " -- " + page);
-    if (hash && hash.length > 0 && this.anchorRefs.has(hash)){// && !StoreService.getInstance().getStore()?.getState().app.menuOpen){ //TODO maybe no more needed
-      // console.log("SCROLL basepagecomponent doscroll");
-      // console.log(this.anchorRefs.get(hash)!);
-      // console.log(PAGE_ROOT_ID);
-      // document.getElementById(PAGE_ROOT_ID)!.scrollTo({top: this.anchorRefs.get(hash)!.current.offsetTop, behavior: 'smooth'});
-      // console.log(this.anchorRefs);
+    if (hash && hash.length > 0 && this.anchorRefs.has(hash)){
       document.getElementById(PAGE_ROOT_ID)!.scrollTop = this.anchorRefs.get(hash)!.current.offsetTop;
-      // console.log("scrolled");
     }
   }
   
