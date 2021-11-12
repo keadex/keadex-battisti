2136ddbb5924de1cdacc1913eb1a6afbe844d56f
Keadex <12099977+keadex@users.noreply.github.com>
2021-11-12 14:03:34 +0100

    fix(frontend): 🐛 fixed GA integration

--- packages/frontend/src/core/google-analytics.ts
+++ packages/frontend/src/core/google-analytics.ts
@@ -2,26 +2,29 @@ import ReactGA from 'react-ga4'
 
 //-------- initGA
 export const initGA = () => {
-  //console.debug(`GA init ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}`);
-  ReactGA.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!, { 'anonymize_ip': true, send_page_view: false });
+  // console.debug(`GA init ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}`);
+  ReactGA.initialize([
+    {
+      trackingId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!,
+      gtagOptions: { 'anonymize_ip': true, send_page_view: false }
+    }
+  ]);
 }
 
 
 //-------- logPageView
 export const logPageView = () => {
-  console.debug(`Logging pageview for ${window.location.pathname}`)
-  ReactGA.gtag('event', 'page_view', {
-    page_location: window.location.href,
-    page_path: window.location.pathname
-  })
+  // console.debug(`Logging pageview for ${window.location.pathname}`);
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 }
 
 
 //-------- logEvent
 export const logEvent = (category = '', action = '') => {
   if (category && action) {
-    ReactGA.gtag('event', action, {
-      'event_category': category
+    ReactGA.event({
+      category: category,
+      action: action
     });
   }
 }
@@ -30,9 +33,12 @@ export const logEvent = (category = '', action = '') => {
 //-------- logException
 export const logException = (description = '', fatal = false) => {
   if (description) {
-    ReactGA.gtag('event', 'exception', {
-      'description': description,
-      'fatal': fatal
+    ReactGA.event({
+      action: "exception",
+      category: ""
+    }, {
+      description: description,
+      fatal: fatal
     });
   }
 }
